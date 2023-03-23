487f34d9c116e4f9dae9dcc1ec675b3759ce506c
Brian <brianvoo@gmail.com>
2023-03-23 12:55:04 +0800

    Truck views to list, update and delete

--- driver/urls.py
+++ driver/urls.py
@@ -1,22 +1,31 @@
 from django.urls import path
-from .views import DriverList, DriverDetail, DriverCreate, TruckCreate, TruckList, TruckDetail
+from .views import DriverList, DriverDetail, DriverCreate, TruckCreate, TruckList, TruckDetail, DriverUpdateDelete, TruckUpdateDelete
 
 urlpatterns = [
-    # Return a list of available drivers, allow filtering using a driver's email, mobile_number, langeuage and his truck's number_plate
-    path('driver/', DriverList.as_view()),
+    # Driver
+    # Create a single driver
+    path('driver/create/', DriverCreate.as_view()),
 
     # Return a single driver
     path('driver/<int:pk>/', DriverDetail.as_view()),
 
-    # Create a single driver
-    path('driver/create/', DriverCreate.as_view()),
+    # Return a list of available drivers, allow filtering using a driver's email, mobile_number, langeuage and his truck's number_plate
+    path('driver/', DriverList.as_view()),
 
+    # Update single driver
+    path('driver/<int:pk>/update/', DriverUpdateDelete.as_view()),
+
+
+    # Truck
     # Create a single truck
     path('truck/create/', TruckCreate.as_view()),
 
-    # View truck ID
+    # View single truck
     path('truck/<int:pk>/', TruckDetail.as_view()),
 
     # Return list of trucks
     path('truck/', TruckList.as_view()),
+
+    # Update single truck
+    path('truck/<int:pk>/update/', TruckUpdateDelete.as_view()),
 ]
--- driver/views.py
+++ driver/views.py
@@ -3,6 +3,17 @@ from rest_framework import generics
 from .models import Truck, Driver
 from .serializers import TruckSerializer, DriverSerializer
 
+# Driver
+# Create a single driver
+class DriverCreate(generics.CreateAPIView):
+    serializer_class = DriverSerializer
+    queryset = Driver.objects.all()
+
+# Return a single driver
+class DriverDetail(generics.RetrieveAPIView):
+    serializer_class = DriverSerializer
+    queryset = Driver.objects.all()
+
 # Return a list of available drivers, allow filtering using a driver's email, mobile_number, langeuage and his truck's number_plate
 class DriverList(generics.ListAPIView):
     serializer_class = DriverSerializer
@@ -14,27 +25,29 @@ class DriverList(generics.ListAPIView):
             queryset = queryset.filter(driverName=driver)
         return queryset
 
-# Return a single driver, added feature to update and delete
-class DriverDetail(generics.RetrieveUpdateDestroyAPIView):
+# Update or delete single driver
+class DriverUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DriverSerializer
     queryset = Driver.objects.all()
 
-# Create a single driver
-class DriverCreate(generics.CreateAPIView):
-    serializer_class = DriverSerializer
-    queryset = Driver.objects.all()
 
+# Truck
 # Create a single truck
 class TruckCreate(generics.CreateAPIView):
     serializer_class = TruckSerializer
     queryset = Truck.objects.all()
 
-# Return single truck, added feature to update and delete
-class TruckDetail(generics.RetrieveUpdateDestroyAPIView):
+# Return single truck
+class TruckDetail(generics.RetrieveAPIView):
     serializer_class = TruckSerializer
     queryset = Truck.objects.all()
 
 # Return list of trucks
 class TruckList(generics.ListAPIView):
+    serializer_class = TruckSerializer
+    queryset = Truck.objects.all()
+
+# Update or delete single truck
+class TruckUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TruckSerializer
     queryset = Truck.objects.all()
