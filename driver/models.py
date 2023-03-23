fdecce242694fe200c74970eb58088c5862f89c5
brianvoo <brianvoo@gmail.com>
2023-03-23 21:06:35 +0800

    added language in driver

--- driver/models.py
+++ driver/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Truck(models.Model):
     number_plate = models.CharField(max_length=50, unique=True)
     registration_plate = models.CharField(max_length=50, unique=True)
+    date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.number_plate
